7fc3e6016d49a9087baa3a40d365df652212311d
pnexer <pnexer@excilys.com>
2018-03-16 17:59:14 +0100

    ajout junit

---
+++ src/packageTest/ComputerDAOTest.java
@@ -0,0 +1,49 @@
+package packageTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ComputerDAOTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSelectListComputer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectComputer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateComputer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteComputer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateComputer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCloseConnection() {
+		fail("Not yet implemented");
+	}
+
+}
